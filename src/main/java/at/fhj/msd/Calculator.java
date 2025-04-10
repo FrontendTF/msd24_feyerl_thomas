02935e02a55d40fb622df20a480c6082d2252615
Thomas Feyerl <thomas.feyerl@edu.fh-joanneum.at>
2025-04-10 22:03:48 +0200

    added log stuff to .gitignore, added ex5_2, updated pom.xl

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -1,24 +1,38 @@
 package at.fhj.msd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Calculator {
+
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
+    
       // Addition
       public double add(double number1, double number2) {
+        logger.debug("number1: " + number1 + " | " + "number2: " + number2);
         return number1 + number2;
     }
 
     // Subtraktion
     public double minus(double number1, double number2) {
+        logger.debug("number1: " + number1 + " | " + "number2: " + number2);
         return number1 - number2;
     }
 
     // Multiplikation
     public double multiply(double number1, double number2) {
+
+        logger.info("multiply called with: number1="+ number1 + ", number2=" + number2);
+        logger.debug("number1: " + number1 + " * " + "number2: " + number2);
+
         return number1 * number2;
     }
 
     // Division
     public double divide(double number1, double number2) {
+
         if (number2 == 0) {
+            logger.error("Divison by 0!");
             throw new ArithmeticException("Division by zero is not allowed!");
         }
         return number1 / number2;
